ee0aa78b853fa09c71639c707df97a7dcc3ded58
Adrian <adrian.martineztr@elorrieta-errekamari.com>
2020-11-26 08:53:52 +0100

    Comentario

---
+++ LaPrueba/src/main/java/LaPrueba/Prueba1.java
@@ -0,0 +1,19 @@
+package LaPrueba;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+		int numero= (int) (Math.random()*6) ;
+		System.out.println(aleatorio(numero));
+	}
+
+	public static String aleatorio(int numero) {
+		String texto;
+		if (numero<2) {
+			texto="menor";
+		}else {
+			texto="mayor";
+		}
+		return texto;
+	}
+}
